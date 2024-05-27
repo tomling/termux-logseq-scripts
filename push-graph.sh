5d68a2fbb8961a4518029a8b4f84db992dec94bb
Tom <19222781+tomling@users.noreply.github.com>
2024-05-27 22:32:58 +0100

    Add scripts

---
+++ push-graph.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+cd ~/storage/shared/Documents/notes
+git add -A
+git commit -m "Sync from SM-xxx @ $(date + '%F %T')" # Source computer and date
+git push
---
+++ src/pull-graph.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+# Replace /notes with the path to your notes directory
+cd ~/storage/shared/Documents/notes
+git pull
